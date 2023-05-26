ea2b26a336b26d9b8c2cd285eb78460ab2f210a2
yunjungyoun <30701004@naver.com>
2023-05-26 21:19:17 +0900

    laout 구성

--- src/main/java/com/godcoder/myhome/MyhomeApplication.java
+++ src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyhomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyhomeApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(MyhomeApplication.class, args);}
+
 	}
 
-}
+
